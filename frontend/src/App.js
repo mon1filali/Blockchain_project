f7b1fc3521ce19b38613bcaeb82cc216e0e87f13
mon1filali <monafilali2003@gmail.com>
2026-06-07 02:44:45 +0100

    modification de blockchainInfo.jsx et aussi creation de composants transactionInfo.jsx qui contient les infos de derniers transaction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import Exercice1 from "./Pages/Exercice1";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/exercice1" element={<Exercice1 />} />
+        
       </Routes>
     </BrowserRouter>
   );
